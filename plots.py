3c11a268c19d1a28fe56c9e6464570348bfabcfd
Eduardo Blancas Reyes <edu@cacaolabs.co>
2015-09-04 11:31:07 -0500

    first commit

---
+++ plots.py
@@ -0,0 +1,118 @@
+#Numpy and sklearn
+import numpy as np
+#Metric for confusion matrix
+from sklearn.metrics import confusion_matrix
+#Metric for ROC
+from sklearn.metrics import roc_curve, auc
+#Metric for Precision-Recall
+from sklearn.metrics import precision_recall_curve, average_precision_score
+#Matplotlib
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+#Confusion matrix
+#http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def plot_confusion_matrix(y_test, y_pred, target_names, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    cm = confusion_matrix(y_test, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    np.set_printoptions(precision=2)
+    fig = Figure()
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(111)
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    fig.colorbar(im)
+    tick_marks = np.arange(len(target_names))
+    ax.set_xticks(tick_marks)
+    ax.set_xticklabels(target_names, rotation=45)
+    ax.set_yticks(tick_marks)
+    ax.set_yticklabels(target_names)
+    fig.tight_layout()
+    ax.set_title(title)
+    ax.set_ylabel('True label')
+    ax.set_xlabel('Predicted label')
+    return fig
+
+#Receiver operating characteristic (ROC)
+#http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html
+def plot_roc_curve(y_true, y_score, n_classes, title="ROC curve"):
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    if n_classes>2:
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+        # Compute micro-average ROC curve and ROC area
+        fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_score.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    else:
+        fpr[1], tpr[1], _ = roc_curve(y_true, y_score)
+        roc_auc[1] = auc(fpr[1], tpr[1])
+    
+    # Plot of a ROC curve for class 1 if binary classifier
+    # Plot all classes and micro-average if multiclass classifier
+    fig = Figure()
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(111)
+    if n_classes==2:
+        ax.plot(fpr[1], tpr[1], label='ROC curve (area = %0.2f)' % roc_auc[1])
+    else:
+        ax.plot(fpr["micro"], tpr["micro"], label='micro-average ROC curve (area = {0:0.2f})'.format(roc_auc["micro"]))
+        for i in range(n_classes):
+            ax.plot(fpr[i], tpr[i], label='ROC curve (area = %0.2f)' % roc_auc[i])
+
+    ax.plot([0, 1], [0, 1], 'k--')
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('Receiver operating characteristic example')
+    ax.legend(loc="lower right")
+    return fig
+
+#Receiver operating characteristic (ROC) with cross validation
+#http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html
+
+#Precision-recall
+#http://scikit-learn.org/stable/auto_examples/model_selection/plot_precision_recall.html
+def plot_precision_recall_curve(y_true, y_score, n_classes, title="Precision-Recall curve"):
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+
+    if n_classes>2:
+        for i in range(n_classes):
+            precision[i], recall[i], _ = precision_recall_curve(y_true[:, i], y_score[:, i])
+            average_precision[i] = average_precision_score(y_true[:, i], y_score[:, i])
+        # Compute micro-average ROC curve and ROC area
+        precision["micro"], recall["micro"], _ = precision_recall_curve(y_true.ravel(), y_score.ravel())
+        average_precision["micro"] = average_precision_score(y_true, y_score, average="micro")
+    else:
+        precision[1], recall[1], _ = precision_recall_curve(y_true, y_score)
+        average_precision[1] = average_precision_score(y_true, y_score)
+    
+    # Plot of a ROC curve for class 1 if binary classifier
+    # Plot all classes and micro-average if multiclass classifier
+    fig = Figure()
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(111)
+    if n_classes==2:
+        ax.plot(recall[1], precision[1], label='Precision-Recall curve')
+    else:
+        ax.plot(recall["micro"], precision["micro"], label='micro-average Precision-recall curve (area = {0:0.2f})'.format(average_precision["micro"]))
+        for i in range(n_classes):
+            ax.plot(recall[i], precision[i], label='P-R curve of class {0} (area = {1:0.2f})'.format(i, average_precision[i]))
+
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    if n_classes==2:
+        ax.set_title('Precision-Recall curve: AUC={0:0.2f}'.format(average_precision[1]))
+    else:
+        ax.set_title(title)
+    ax.legend(loc="lower right")
+    return fig
