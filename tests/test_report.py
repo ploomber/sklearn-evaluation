c1f758923002ef409150885f9adf8bec74fd212f
Eduardo Blancas Reyes <edu@cacaolabs.co>
2015-10-14 10:48:06 -0500

    adds tests when report wants to be generated using a model with no importances_features_ attr

--- tests/test_report.py
+++ tests/test_report.py
@@ -34,7 +34,31 @@ class Test_Complete_Classification_Report(TestCase):
         dummy_report = dummy_report_file.read()
         #Compare report
         self.assertEqual(report, dummy_report)
-    def test_default_template_no_feature_list(self):
+    def test_default_template_no_feature_importances(self):
+        #Load a model
+        model = joblib.load(os.path.join(models_path,'classifier_without_feature_importances_model.pkl'))
+        #Load y_true
+        y_true = joblib.load(os.path.join(models_path,'classifier_without_feature_importances_y_true.pkl'))
+        #Load y_pred
+        y_pred = joblib.load(os.path.join(models_path,'classifier_without_feature_importances_y_pred.pkl'))
+        #Load y_score
+        y_score = joblib.load(os.path.join(models_path,'classifier_without_feature_importances_y_score.pkl'))
+        #Load X
+        X = joblib.load(os.path.join(models_path,'classifier_without_feature_importances_x.pkl'))
+        #Generate feature list
+        feature_list = range(X.shape[1]) #FIND A BETTER WAY TO DO THIS
+        #Generate target names
+        target_names = range(len(set(y_true))) #FIND A BETTER WAY
+        #Report name
+        name = 'test_report_without_feature_importances'
+        rg = ReportGenerator()
+        report = rg.generate_report(model=model, y_true=y_true, y_pred=y_pred, y_score=y_score, feature_list=feature_list, target_names=target_names, name=name)
+        #Load dummy report
+        dummy_report_file = open(os.path.join(models_path, 'classiffier_without_feature_importances_report.html'))
+        dummy_report = dummy_report_file.read()
+        #Compare report
+        self.assertEqual(report, dummy_report)
+    def test_default_template_no_model(self):
         pass
     def test_default_template_no_scores(self):
         pass
