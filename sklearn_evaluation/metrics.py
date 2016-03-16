9efeb8c6a1c268b818bb404a53a633825108afee
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-03-16 11:21:10 -0500

    changes percent parameter to proportion

--- sklearn_evaluation/metrics.py
+++ sklearn_evaluation/metrics.py
@@ -1,17 +1,17 @@
 import numpy as np
 from sklearn.metrics import precision_score
 
-def precision_at(labels, scores, percent, ignore_nas=False):
+def precision_at(labels, scores, proportion, ignore_nas=False):
     '''
-    Calculates precision at a given percent.
+    Calculates precision at a given proportion.
     Only supports binary classification.
     '''
     #Sort scores in descending order    
     scores_sorted = np.sort(scores)[::-1]
 
-    #Based on the percent, get the index to split the data
+    #Based on the proportion, get the index to split the data
     #if value is negative, return 0
-    cutoff_index = max(int(len(labels) * percent) - 1, 0)
+    cutoff_index = max(int(len(labels) * proportion) - 1, 0)
     #Get the cutoff value
     cutoff_value = scores_sorted[cutoff_index]
 
@@ -26,18 +26,18 @@ def precision_at(labels, scores, percent, ignore_nas=False):
     return precision, cutoff_value
 
 
-def __threshold_at(y_score, percent):
+def __threshold_at(y_score, proportion):
     #Sort scores in descending order    
     scores_sorted = np.sort(y_score)[::-1]
-    #Based on the percent, get the index to split the data
+    #Based on the proportion, get the index to split the data
     #if value is negative, return 0
-    threshold_index = max(int(len(y_score) * percent) - 1, 0)
+    threshold_index = max(int(len(y_score) * proportion) - 1, 0)
     #Get the cutoff value
     threshold_value = scores_sorted[threshold_index]
     return threshold_value
 
-def __binarize_scores_at(y_score, percent):
-    threshold_value = __threshold_at(y_score, percent)
+def __binarize_scores_at(y_score, proportion):
+    threshold_value = __threshold_at(y_score, proportion)
     y_score_binary = np.array(map(lambda x: int(x>=threshold_value), y_score))
     return y_score_binary
 
@@ -60,29 +60,29 @@ def __precision(y_true, y_pred):
     precision = precision_score(y_true, y_pred)
     return precision
 
-def tp_at(y_true, y_score, percent):
-    y_pred = __binarize_scores_at(y_score, percent)
+def tp_at(y_true, y_score, proportion):
+    y_pred = __binarize_scores_at(y_score, proportion)
     tp = (y_pred == 1) & (y_true == 1)
     return tp.sum()
 
-def fp_at(y_true, y_score, percent):
-    y_pred = __binarize_scores_at(y_score, percent)
+def fp_at(y_true, y_score, proportion):
+    y_pred = __binarize_scores_at(y_score, proportion)
     fp = (y_pred == 1) & (y_true == 0)
     return fp.sum()
 
-def tn_at(y_true, y_score, percent):
-    y_pred = __binarize_scores_at(y_score, percent)
+def tn_at(y_true, y_score, proportion):
+    y_pred = __binarize_scores_at(y_score, proportion)
     tn = (y_pred == 0) & (y_true == 0)
     return tn.sum()
 
-def fn_at(y_true, y_score, percent):
-    y_pred = __binarize_scores_at(y_score, percent)
+def fn_at(y_true, y_score, proportion):
+    y_pred = __binarize_scores_at(y_score, proportion)
     fn = (y_pred == 0) & (y_true == 1)
     return fn.sum()
 
-def labels_at(y_true, y_score, percent, normalize=False):
+def labels_at(y_true, y_score, proportion, normalize=False):
     '''
-        Return the number of labels encountered in the top  X percent
+        Return the number of labels encountered in the top  X proportion
     '''
     #Get indexes of scores sorted in descending order
     indexes = np.argsort(y_score)[::-1]
@@ -90,12 +90,12 @@ def labels_at(y_true, y_score, percent, normalize=False):
     #Sort true values in the same order
     y_true_sorted = y_true[indexes]
 
-    #Grab top x percent of true values
-    cutoff_index = max(int(len(y_true_sorted) * percent) - 1, 0)
+    #Grab top x proportion of true values
+    cutoff_index = max(int(len(y_true_sorted) * proportion) - 1, 0)
     #add one to index to grab values including that index
     y_true_top = y_true_sorted[:cutoff_index+1]
 
-    #Count the number of non-nas in the top x percent
+    #Count the number of non-nas in the top x proportion
     #we are returning a count so it should be an int
     values = int((~np.isnan(y_true_top)).sum())
 
