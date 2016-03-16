010fac747351679b843bc6bd2c54078ff2084587
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-03-16 11:08:04 -0500

    adds new metrics

--- sklearn_evaluation/metrics.py
+++ sklearn_evaluation/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.metrics import precision_score
 
-def precision_at(labels, scores, percent=0.01):
+def precision_at(labels, scores, percent=0.01, ignore_nas=False):
     '''
     Calculates precision at a given percent.
     Only supports binary classification.
@@ -16,8 +16,90 @@ def precision_at(labels, scores, percent=0.01):
     cutoff_value = scores_sorted[cutoff_index]
 
     #Convert scores to binary, by comparing them with the cutoff value
-    scores_binary = map(lambda x: int(x>=cutoff_value), scores)
+    scores_binary = np.array(map(lambda x: int(x>=cutoff_value), scores))
     #Calculate precision using sklearn function
-    precision = precision_score(labels, scores_binary)
+    if ignore_nas:
+        precision = __precision(labels, scores_binary)
+    else:
+        precision = precision_score(labels, scores_binary)
 
     return precision, cutoff_value
+
+
+def __threshold_at_percent(y_score, percent):
+    #Sort scores in descending order    
+    scores_sorted = np.sort(y_score)[::-1]
+    #Based on the percent, get the index to split the data
+    #if value is negative, return 0
+    threshold_index = max(int(len(y_score) * percent) - 1, 0)
+    #Get the cutoff value
+    threshold_value = scores_sorted[threshold_index]
+    return threshold_value
+
+def __binarize_scores_at_percent(y_score, percent):
+    threshold_value = __threshold_at_percent(y_score, percent)
+    y_score_binary = np.array(map(lambda x: int(x>=threshold_value), y_score))
+    return y_score_binary
+
+def __precision(y_true, y_pred):
+    '''
+        Precision metric tolerant to unlabeled data in y_true,
+        NA values are ignored for the precision calculation
+    '''
+    #make copies of the arrays to avoid modifying the original ones
+    y_true = np.copy(y_true)
+    y_pred = np.copy(y_pred)
+
+    #precision = tp/(tp+fp)
+    #True nehatives do not affect precision value, so for every missing
+    #value in y_true, replace it with 0 and also replace the value
+    #in y_pred with 0
+    is_nan = np.isnan(y_true)
+    y_true[is_nan] = 0
+    y_pred[is_nan] = 0
+    precision = precision_score(y_true, y_pred)
+    return precision
+
+def tp_at_percent(y_true, y_score, percent):
+    y_pred = __binarize_scores_at_percent(y_score, percent)
+    tp = (y_pred == 1) & (y_true == 1)
+    return tp.sum()
+
+def fp_at_percent(y_true, y_score, percent):
+    y_pred = __binarize_scores_at_percent(y_score, percent)
+    fp = (y_pred == 1) & (y_true == 0)
+    return fp.sum()
+
+def tn_at_percent(y_true, y_score, percent):
+    y_pred = __binarize_scores_at_percent(y_score, percent)
+    tn = (y_pred == 0) & (y_true == 0)
+    return tn.sum()
+
+def fn_at_percent(y_true, y_score, percent):
+    y_pred = __binarize_scores_at_percent(y_score, percent)
+    fn = (y_pred == 0) & (y_true == 1)
+    return fn.sum()
+
+def labels_at_percent(y_true, y_score, percent, normalize=False):
+    '''
+        Return the number of labels encountered in the top  X percent
+    '''
+    #Get indexes of scores sorted in descending order
+    indexes = np.argsort(y_score)[::-1]
+
+    #Sort true values in the same order
+    y_true_sorted = y_true[indexes]
+
+    #Grab top x percent of true values
+    cutoff_index = max(int(len(y_true_sorted) * percent) - 1, 0)
+    #add one to index to grab values including that index
+    y_true_top = y_true_sorted[:cutoff_index+1]
+
+    #Count the number of non-nas in the top x percent
+    #we are returning a count so it should be an int
+    values = int((~np.isnan(y_true_top)).sum())
+
+    if normalize:
+        values = float(values)/(~np.isnan(y_true)).sum()
+        
+    return values
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,16 +1,266 @@
 from unittest import TestCase
-from sklearn_evaluation.metrics import precision_at
+from sklearn_evaluation.metrics import (precision_at, labels_at_percent,
+    tp_at_percent, fp_at_percent)
 
-class Test_Precision_At(TestCase):
+from sklearn_evaluation.metrics import __threshold_at_percent as threshold_at_percent
+from sklearn_evaluation.metrics import __binarize_scores_at_percent as binarize_scores_at_percent
+
+import numpy as np
+from numpy import nan
+from random import shuffle
+
+class Test_threshold_at_percent(TestCase):
+    def setUp(self):
+        self.scores = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        shuffle(self.scores)
+
+    def test_at_10(self):
+        threshold = threshold_at_percent(self.scores, 0.1)
+        self.assertEqual(threshold, 1.0)
+
+    def test_at_50(self):
+        threshold = threshold_at_percent(self.scores, 0.5)
+        self.assertEqual(threshold, 0.6)
+
+    def test_at_100(self):
+        threshold = threshold_at_percent(self.scores, 1.0)
+        self.assertEqual(threshold, 0.1)
+
+class Test_binarize_scores_at_percent(TestCase):
+    def setUp(self):
+        self.scores = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+
+    def test_at_10(self):
+        binary_scores = binarize_scores_at_percent(self.scores, 0.1)
+        expected = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        np.testing.assert_equal(binary_scores, expected)
+
+    def test_at_50(self):
+        binary_scores = binarize_scores_at_percent(self.scores, 0.5)
+        expected = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
+        np.testing.assert_equal(binary_scores, expected)
+
+    def test_at_100(self):
+        binary_scores = binarize_scores_at_percent(self.scores, 1.0)
+        expected = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        np.testing.assert_equal(binary_scores, expected)
+
+
+class Test_precision_at(TestCase):
     def test_perfect_precision(self):
-        labels = [1  ,1 ,1 ,1 ,1 ,0 ,0 ,0 ,0 ,0]
-        scores = [100,90,80,70,60,50,40,30,20,10]
-        prec, cutoff = precision_at(labels, scores, 0.10)
+        labels = np.array([1  ,1 ,1 ,1 ,1 ,0 ,0 ,0 ,0 ,0])
+        scores = np.array([100,90,80,70,60,50,40,30,20,10])
+        prec, cutoff = precision_at(labels, scores, percent=0.10)
         self.assertEqual(prec, 1.0)
         self.assertEqual(cutoff, 100)
+
+    def test_perfect_precision_with_nas(self):
+        labels = np.array([1, nan, 1, 1, 1 , nan, 0, 0, 0, 0])
+        scores = np.array([100,90,80,70,60,50,40,30,20,10])
+        prec, cutoff = precision_at(labels, scores, percent=0.10, ignore_nas=True)
+        self.assertEqual(prec, 1.0)
+        self.assertEqual(cutoff, 100)
+
     def test_baseline_precision(self):
-        labels = [1  ,1 ,1 ,1 ,1 ,0 ,0 ,0 ,0 ,0]
-        scores = [100,90,80,70,60,50,40,30,20,10]
-        prec, cutoff = precision_at(labels, scores, 1.0)
+        labels = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
+        scores = np.array([100,90,80,70,60,50,40,30,20,10])
+        prec, cutoff = precision_at(labels, scores, percent=1.0)
         self.assertEqual(prec, 0.5)
         self.assertEqual(cutoff, 10)
+
+    def test_baseline_precision_with_nas(self):
+        labels = np.array([nan, 1, nan, 1, 1, nan, nan, 0, 0, 0])
+        scores = np.array([100,90,80,70,60,50,40,30,20,10])
+        prec, cutoff = precision_at(labels, scores, percent=1.0, ignore_nas=True)
+        self.assertEqual(prec, 0.5)
+        self.assertEqual(cutoff, 10)
+
+
+class Test_labels_at_percent(TestCase):
+    def test_no_labels_at_1(self):
+        y_true = np.array([nan, nan, nan, nan, nan, nan, nan, nan, nan, nan])
+        y_score = np.random.rand(1, 10)
+        labels = labels_at_percent(y_true, y_score, percent=0.01, normalize=False)
+        self.assertEqual(labels, 0)
+
+    def test_no_labels_at_50(self):
+        y_true = np.array([nan, nan, nan, nan, nan, nan, nan, nan, nan, nan])
+        y_score = np.random.rand(1, 10)
+        labels = labels_at_percent(y_true, y_score, percent=0.5, normalize=False)
+        self.assertEqual(labels, 0)
+
+    def test_no_labels_at_100(self):
+        y_true = np.array([nan, nan, nan, nan, nan, nan, nan, nan, nan, nan])
+        y_score = np.random.rand(1, 10)
+        labels = labels_at_percent(y_true, y_score, percent=1.0, normalize=False)
+        self.assertEqual(labels, 0)
+
+    def test_one_label_at_10(self):
+        y_true = np.array([1, nan, nan, nan, nan, nan, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.1, normalize=False)
+        self.assertEqual(labels, 1)
+
+    def test_one_label_at_10_norm(self):
+        y_true = np.array([1, nan, nan, nan, nan, nan, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.1, normalize=True)
+        self.assertEqual(labels, 1.0)
+
+    def test_one_label_at_50(self):
+        y_true = np.array([1, nan, nan, nan, nan, nan, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.5, normalize=False)
+        self.assertEqual(labels, 1)
+
+    def test_one_label_at_100(self):
+        y_true = np.array([1, nan, nan, nan, nan, nan, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=1.0, normalize=False)
+        self.assertEqual(labels, 1)
+
+    def test_60_labels_at_60(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.6, normalize=False)
+        self.assertEqual(labels, 6)
+
+    def test_60_labels_at_60_norm(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.6, normalize=True)
+        self.assertEqual(labels, 1.0)
+
+    def test_60_labels_at_60_mixed_values(self):
+        y_true = np.array([1, 0, 0, 1, 0, 1, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.6, normalize=False)
+        self.assertEqual(labels, 6)
+
+    def test_60_labels_at_60_norm_mixed_values(self):
+        y_true = np.array([0, 0, 0, 1, 0, 1, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.6, normalize=True)
+        self.assertEqual(labels, 1.0)
+
+    def test_60_labels_at_30(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.3, normalize=False)
+        self.assertEqual(labels, 3)
+
+    def test_60_labels_at_30_norm(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, nan, nan, nan, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        labels = labels_at_percent(y_true, y_score, percent=0.3, normalize=True)
+        self.assertEqual(labels, 0.5)
+
+class Test_tp_at_percent(TestCase):
+    def test_with_nas(self):
+        y_true = np.array([1, nan, 1, 1, 1, 1, 1, 1, 1, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=0.1)
+        self.assertEqual(tps, 1)
+
+    def test_all_tp_at_10(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=0.1)
+        self.assertEqual(tps, 1)
+
+    def test_all_tp_at_50(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=0.5)
+        self.assertEqual(tps, 5)
+
+    def test_all_tp_at_100(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=1.0)
+        self.assertEqual(tps, 10)
+
+    def test_no_tp_at_50(self):
+        y_true = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=0.5)
+        self.assertEqual(tps, 0)
+
+    def test_no_tp_at_100(self):
+        y_true = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=1.0)
+        self.assertEqual(tps, 0)
+
+    def test_some_tp_at_10(self):
+        y_true = np.array([1, 0, 0, 0, 0, 0, 0, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=0.1)
+        self.assertEqual(tps, 1)
+
+    def test_some_tp_at_50(self):
+        y_true = np.array([1, 1, 0, 0, 1, 0, 0, 1, 1, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=0.5)
+        self.assertEqual(tps, 3)
+
+    def test_some_tp_at_100(self):
+        y_true = np.array([0, 0, 0, 0, 1, 0, 0, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        tps = tp_at_percent(y_true, y_score, percent=1.0)
+        self.assertEqual(tps, 4)
+
+class Test_fp_at_percent(TestCase):
+    def test_with_nas(self):
+        y_true = np.array([0, nan, 1, 1, 1, 1, 1, 1, 1, nan])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=0.1)
+        self.assertEqual(fps, 1)
+
+    def test_all_fp_at_10(self):
+        y_true = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=0.1)
+        self.assertEqual(fps, 1)
+
+    def test_all_fp_at_50(self):
+        y_true = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=0.5)
+        self.assertEqual(fps, 5)
+
+    def test_all_fp_at_100(self):
+        y_true = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=1.0)
+        self.assertEqual(fps, 10)
+
+    def test_no_fp_at_50(self):
+        y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=0.5)
+        self.assertEqual(fps, 0)
+
+    def test_no_fp_at_100(self):
+        y_true = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=1.0)
+        self.assertEqual(fps, 0)
+
+    def test_some_fp_at_10(self):
+        y_true = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=0.1)
+        self.assertEqual(fps, 1)
+
+    def test_some_fp_at_50(self):
+        y_true = np.array([1, 1, 0, 0, 1, 0, 0, 1, 1, 0])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=0.5)
+        self.assertEqual(fps, 2)
+
+    def test_some_fp_at_100(self):
+        y_true = np.array([0, 0, 0, 0, 1, 0, 0, 1, 1, 1])
+        y_score = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        fps = fp_at_percent(y_true, y_score, percent=1.0)
+        self.assertEqual(fps, 6)
