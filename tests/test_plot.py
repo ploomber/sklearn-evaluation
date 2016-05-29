bfdd67edb6299780dddbfe80702f908fe023f3d9
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-05-28 19:17:46 -0500

    adds tests to check that plot functions return the same result when y_score is a vector

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -9,8 +9,9 @@ feature_names = range(4)
 feature_importances = np.array([0.5, 0.4, 0.3, 0.2])
 y_test = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
 y_pred = np.array([1, 0, 1, 1, 1, 0, 0, 0, 0, 0])
-y_score = np.array([[0.5, 0.6, 0.7, 0.8, 0.9, 0.9, 0.8, 0.1, 0.1, 0.3],
+y_score = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                    [0.5, 0.6, 0.7, 0.8, 0.9, 0.9, 0.8, 0.1, 0.1, 0.3]]).T
+y_score_vector = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 0.9, 0.8, 0.1, 0.1, 0.3])
 
 
 @image_comparison(baseline_images=['confusion_matrix'],
@@ -34,11 +35,25 @@ def test_roc():
     plot.roc(y_test, y_score)
 
 
+@image_comparison(baseline_images=['roc'],
+                  extensions=['png'],
+                  remove_text=True)
+def test_roc_y_score_vector():
+    plot.roc(y_test, y_score_vector)
+
+
 @image_comparison(baseline_images=['precision_recall'],
                   extensions=['png'],
                   remove_text=True)
 def test_precision_recall():
     plot.precision_recall(y_test, y_score)
+
+
+@image_comparison(baseline_images=['precision_recall'],
+                  extensions=['png'],
+                  remove_text=True)
+def test_precision_recall_y_score_vector():
+    plot.precision_recall(y_test, y_score_vector)
 
 
 # @image_comparison(baseline_images=['feature_importances'],
@@ -53,3 +68,10 @@ def test_precision_recall():
                   remove_text=True)
 def test_precision_at_proportions():
     plot.precision_at_proportions(y_test, y_score)
+
+
+@image_comparison(baseline_images=['precision_at_proportions'],
+                  extensions=['png'],
+                  remove_text=True)
+def test_precision_at_proportions_y_score_vector():
+    plot.precision_at_proportions(y_test, y_score_vector)
