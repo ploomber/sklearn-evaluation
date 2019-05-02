122649eae2c7733f2ce2499c460a1815a41809a3
Eduardo Blancas Reyes <fkq8@blancas.io>
2019-05-01 21:46:31 -0400

    updates example

--- examples/report.py
+++ examples/report.py
@@ -1,24 +1,17 @@
 from pathlib import Path
 
-
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
 from sklearn_evaluation import ClassifierEvaluator
 
-
-data = datasets.make_classification(200, 10, 5, class_sep=0.65)
-X = data[0]
-y = data[1]
-
+X, y = datasets.make_classification(200, 10, 5, class_sep=0.65)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-est = RandomForestClassifier()
-est.fit(X_train, y_train)
+model = RandomForestClassifier(n_estimators=5).fit(X_train, y_train)
 
-evaluator = ClassifierEvaluator(est, y_true=y_test, X=X_test)
-report = evaluator.generate_report()
+evaluator = ClassifierEvaluator(model, y_true=y_test, X=X_test)
+report = evaluator.make_report()
 
 p = Path('~/Desktop/report.html').expanduser()
 report.save(p)
