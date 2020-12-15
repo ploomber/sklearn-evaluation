bedd80e52cb2b9d62fcdb5ad1569d60d594a9ec8
Eduardo Blancas Reyes <github@blancas.io>
2020-12-14 18:54:13 -0600

    adds missing args for reorting coverage

--- noxfile.py
+++ noxfile.py
@@ -9,17 +9,20 @@ def tests(session):
 
     # run unit tests and output coverage stats
     session.run('pytest', 'tests/', '--cov=sklearn_evaluation')
-    session.run('coveralls')
 
     # run tests in docstrings
     # pytest doctest docs: https://docs.pytest.org/en/latest/doctest.html
     # doctest docs: https://docs.python.org/3/library/doctest.html
-    session.run('pytest', 'src/', '--doctest-modules')
+    session.run('pytest', 'src/', '--doctest-modules',
+                '--cov=sklearn_evaluation')
 
     # run examples (this is a hacky way to do it since --doctest-modules will
     # first load any .py files, which are the examples, and then try to run
     # any doctests, there isn't any)
-    session.run('pytest', 'examples/', '--doctest-modules')
+    session.run('pytest', 'examples/', '--doctest-modules',
+                '--cov=sklearn_evaluation')
+
+    session.run('coveralls')
 
     if session.python == '3.7':
         session._run('conda', 'env', 'update', '--prefix',
