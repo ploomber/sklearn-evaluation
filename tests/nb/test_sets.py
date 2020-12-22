874669490b7dade0e9c8ec4ef590bbc5c7886c01
Eduardo Blancas Reyes <github@blancas.io>
2020-12-22 17:33:05 -0600

    updates test

--- tests/nb/test_sets.py
+++ tests/nb/test_sets.py
@@ -3,6 +3,6 @@ from sklearn_evaluation.nb import sets
 
 def test_differences():
     both, a_only, b_only = sets.differences([1, 2, 3], [1, 4, 5])
-    assert both == {1}
-    assert a_only == {2, 3}
-    assert b_only == {4, 5}
+    assert both == [1]
+    assert a_only == [2, 3]
+    assert b_only == [4, 5]
