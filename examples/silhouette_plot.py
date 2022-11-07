9b45aa47b580b63959951ca7706c50bbd4d0fdb1
Neelasha Sen <neelashasen@192.168.1.21>
2022-11-07 20:09:20 +0530

    Renamed function

--- examples/silhouette_plot.py
+++ examples/silhouette_plot.py
@@ -7,5 +7,5 @@ from sklearn_evaluation import plot
 X, _ = make_blobs(n_samples=100, centers=3, n_features=5, random_state=0)
 kmeans = KMeans(random_state=1)
 
-plot.silhouette_analysis(X, kmeans)
+plot.silhouette_plot(X, kmeans)
 plt.show()
