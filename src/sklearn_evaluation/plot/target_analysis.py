6cfb256edc5f4eefa32a4a5d0547b9e22000edc4
Neelasha Sen <neelashasen@192.168.1.4>
2022-11-18 19:45:22 +0530

    Added license

--- src/sklearn_evaluation/plot/target_analysis.py
+++ src/sklearn_evaluation/plot/target_analysis.py
@@ -1,3 +1,23 @@
+"""
+Plot for target analysis
+
+NOTE: this is based on the yellowbricks target analysis module. License below.
+
+Copyright 2016-2020 The scikit-yb developers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,11 +39,7 @@ def _validate_target(y):
             format(y_type))
 
 
-def target_plot(y_train,
-                y_test=None,
-                labels=None,
-                colors=None,
-                ax=None):
+def target_plot(y_train, y_test=None, labels=None, colors=None, ax=None):
     """Target analysis plot for visualising class imbalance.
 
     There are two modes:
@@ -39,9 +55,11 @@ def target_plot(y_train,
     ----------
     y_train : array-like
         Array or list of shape (n,) that contains discrete data.
+        Refer https://numpy.org/doc/stable/glossary.html#term-array-like
     y_test : array-like, optional
         Array or list of shape (m,) that contains discrete data. If
         specified, the bar chart will be drawn in compare mode.
+        Refer https://numpy.org/doc/stable/glossary.html#term-array-like
 
     labels: list, optional
         A list of class names for the x-axis if the target is already encoded.
@@ -51,9 +69,7 @@ def target_plot(y_train,
         in the data will be used.
 
     colors: list of strings
-        Specify colors for the barchart (will override colormap if both are provided).
-    colormap : string or matplotlib cmap
-        Specify a colormap to color the classes.
+        Specify colors for the barchart.
 
     ax : :class:`matplotlib.axes.Axes`, optional
         The axes upon which to plot the curve. If None, the plot is drawn
@@ -78,11 +94,9 @@ def target_plot(y_train,
     if labels is not None:
         if len(labels) != len(classes_):
             raise ValueError(
-                (
-                    "discovered {} classes in the data, does not match "
-                    "the {} labels specified."
-                ).format(len(classes_), len(labels))
-            )
+                ("Discovered {} classes in the data, does not match "
+                 "the {} labels specified.").format(len(classes_),
+                                                    len(labels)))
 
     if ax is None:
         ax = plt.gca()
@@ -92,27 +106,26 @@ def target_plot(y_train,
         ax.bar(
             np.arange(len(support_)),
             support_,
-            color=colors,
+            color=colors if colors else "#0070FF",
             align="center",
             width=0.5,
         )
     else:
-        support_ = np.array(
-            [[(y == idx).sum() for idx in classes_] for y in targets]
-        )
+        support_ = np.array([[(y == idx).sum() for idx in classes_]
+                             for y in targets])
         bar_width = 0.35
         legends = ["train", "test"]
-
+        colors = colors if colors else ['#0070FF', '#FF9B00']
         for idx, support in enumerate(support_):
             index = np.arange(len(classes_))
             if idx > 0:
                 index = index + bar_width
 
-            ax.bar(
-                index, support, bar_width,
-                color=colors[idx],
-                label=legends[idx]
-            )
+            ax.bar(index,
+                   support,
+                   bar_width,
+                   color=colors[idx],
+                   label=legends[idx])
 
     ax.set_title("Class Balance for {:,} Instances".format(support_.sum()))
 
@@ -125,7 +138,7 @@ def target_plot(y_train,
     ax.set_xticks(xticks)
     ax.set_xticklabels(labels)
 
-    # Compute the ceil ing for the y limit
+    # Compute the ceiling for the y limit
     cmax = support_.max()
     ax.set_ylim(0, cmax + cmax * 0.1)
     ax.set_ylabel("support")
--- tests/test_target_analysis.py
+++ tests/test_target_analysis.py
@@ -1,10 +1,37 @@
+"""
+Some of the test cases are based on the yellowbricks test module.
+
+License below
+
+Copyright 2016-2020 The scikit-yb developers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
 import pytest
 
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets
+
 from matplotlib.testing.decorators import image_comparison
 
+from sklearn.model_selection import train_test_split
+
 from sklearn_evaluation import plot
+
+iris_data = datasets.load_iris()
 
 
 def test_invalid_target():
@@ -27,11 +54,12 @@ def test_class_names_must_match(target_analysis_binary):
     _, _, y_train, y_test = target_analysis_binary
 
     with pytest.raises(ValueError):
-        plot.target_plot(y_train,
-                         y_test=y_test,
-                         labels=["a", "b", "c"])
+        plot.target_plot(y_train, y_test=y_test, labels=["a", "b", "c"])
 
 
+@image_comparison(baseline_images=['binary_balance'],
+                  extensions=['png'],
+                  remove_text=False)
 def test_binary_balance(target_analysis_binary):
     """
     Test binary classification in balance mode
@@ -39,9 +67,11 @@ def test_binary_balance(target_analysis_binary):
 
     _, _, y_train, y_test = target_analysis_binary
     plot.target_plot(y_train)
-    #plt.savefig('binary_balance.png')
 
 
+@image_comparison(baseline_images=['binary_compare'],
+                  extensions=['png'],
+                  remove_text=False)
 def test_binary_compare(target_analysis_binary):
     """
     Test binary classification in compare mode
@@ -51,13 +81,15 @@ def test_binary_compare(target_analysis_binary):
     plot.target_plot(y_train, y_test)
 
 
+@image_comparison(baseline_images=['multiclass_balance'],
+                  extensions=['png'],
+                  remove_text=False)
 def test_multiclass_balance(target_analysis_multiclass):
     """
-    Test multiclass classification in compare mode
+    Test multiclass classification in balance mode
     """
     _, _, y_train, y_test = target_analysis_multiclass
     plot.target_plot(y_train)
-    #plt.savefig('multilcass_balance.png')
 
 
 @image_comparison(baseline_images=['multiclass_compare'],
@@ -68,6 +100,43 @@ def test_multiclass_compare(target_analysis_multiclass):
     Test multiclass classification in compare mode
     """
     _, _, y_train, y_test = target_analysis_multiclass
-    plot.target_plot(y_train, y_test, colors=['#0070FF','#FF9B00'])
-    #plt.savefig('multiclass_compare.png')
+    plot.target_plot(y_train, y_test)
 
+
+@image_comparison(baseline_images=['multiclass_compare_iris_pandas'],
+                  extensions=['png'],
+                  remove_text=False)
+def test_multiclass_compare_iris_pandas():
+    """
+    Test multiclass classification in compare mode for pandas input type
+    """
+    df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+    df['target'] = pd.Series(iris_data.target)
+    train, test = train_test_split(df, test_size=0.3, random_state=42)
+    plot.target_plot(train.target, test.target)
+
+
+@image_comparison(baseline_images=['iris_labels_compare'],
+                  extensions=['png'],
+                  remove_text=False)
+def test_labels_iris():
+    X = iris_data.data
+    y = iris_data.target
+    _, _, y_train, y_test = train_test_split(X,
+                                             y,
+                                             test_size=0.3,
+                                             random_state=42)
+    plot.target_plot(y_train,
+                     y_test,
+                     labels=['Setosa', 'Versicolour', 'Virginica'])
+
+
+@image_comparison(baseline_images=['bar_color'],
+                  extensions=['png'],
+                  remove_text=False)
+def test_bar_color(target_analysis_binary):
+    """
+    Test multiclass classification in compare mode
+    """
+    _, _, y_train, y_test = target_analysis_binary
+    plot.target_plot(y_train, y_test, colors=['y', 'g'])
