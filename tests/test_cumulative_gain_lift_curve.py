727424f44a5578504541d5b9128dabd9a8ae4f0b
Neelasha Sen <neelashasen@192.168.1.4>
2022-12-01 16:02:28 +0530

    Increased tol for windows

--- tests/test_cumulative_gain_lift_curve.py
+++ tests/test_cumulative_gain_lift_curve.py
@@ -43,7 +43,7 @@ def _convert_labels_into_string(y_true):
 @image_comparison(baseline_images=['string_classes_cumulative_gain'],
                   extensions=['png'],
                   remove_text=True,
-                  tol=6.0)
+                  tol=7.0)
 def test_string_classes_cumulative_gain():
     clf = LogisticRegression()
     clf.fit(X, _convert_labels_into_string(y))
@@ -88,7 +88,7 @@ def test_array_like_cumulative_gain():
 @image_comparison(baseline_images=['string_classes_lift_curve'],
                   extensions=['png'],
                   remove_text=True,
-                  tol=7.0)
+                  tol=8.0)
 def test_string_classes_lift_curve():
     clf = LogisticRegression()
     clf.fit(X, _convert_labels_into_string(y))
