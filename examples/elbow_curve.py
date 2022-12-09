11e2cf7cdca1fc63072673e0322507f2c14aec23
Neelasha Sen <neelashasen@192.168.1.3>
2022-12-10 02:23:06 +0530

    Removed n_init

--- examples/elbow_curve.py
+++ examples/elbow_curve.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn_evaluation import plot
 
 X, _ = make_blobs(n_samples=100, centers=3, n_features=5, random_state=0)
-kmeans = KMeans(random_state=1, n_init='auto')
+kmeans = KMeans(random_state=1)
 
 plot.elbow_curve(X, kmeans, n_clusters=range(1, 30))
 plt.show()
