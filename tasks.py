4bfae9aa6fd2c6788a58b3a7e2834dc0312f66d3
Neelasha Sen <neelashasen@192.168.1.3>
2022-12-10 01:11:23 +0530

    Added dependencies

--- tasks.py
+++ tasks.py
@@ -21,6 +21,9 @@ def setup(c, version=None):
           '&& pip install jupyter-book'
           '&& pip install ghp-import'
           )  # lxml needed for NotebookCollection.py example
+    # jupyter-book and ghp-import added here since it
+    # takes too long to resolve dependencies when added
+    # in setup.py
 
     print(f'Done! Activate your environment with:\nconda activate {env_name}')
 
