a2b619bb0900e07cc39dd74b1064b7dfc441add4
Neelasha Sen <neelashasen@192.168.1.4>
2022-12-24 01:02:17 +0530

    Missing fig

--- examples/ClassificationReport.py
+++ examples/ClassificationReport.py
@@ -4,27 +4,36 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 from sklearn_evaluation import plot
+import matplotlib.pyplot as plt
 
-X, y = datasets.make_classification(200, 10, n_informative=5, class_sep=0.65)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-y_pred_rf = RandomForestClassifier().fit(X_train, y_train).predict(X_test)
-y_pred_lr = LogisticRegression().fit(X_train, y_train).predict(X_test)
+def show_figure(fig):
+    dummy = plt.figure()
+    new_manager = dummy.canvas.manager
+    new_manager.canvas.figure = fig
+    fig.set_canvas(new_manager.canvas)
 
-target_names = ["Not spam", "Spam"]
 
-cr_rf = plot.ClassificationReport.from_raw_data(
-    y_test, y_pred_rf, target_names=target_names
-)
-cr_lr = plot.ClassificationReport.from_raw_data(
-    y_test, y_pred_lr, target_names=target_names
-)
+def main():
+    X, y = datasets.make_classification(200, 10, n_informative=5, class_sep=0.65)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# display one of the classification reports
-cr_rf
+    y_pred_rf = RandomForestClassifier().fit(X_train, y_train).predict(X_test)
+    y_pred_lr = LogisticRegression().fit(X_train, y_train).predict(X_test)
 
-# how better it is the random forest?
-cr_rf - cr_lr
+    target_names = ["Not spam", "Spam"]
 
-# compare both reports
-cr_rf + cr_lr
+    cr_rf = plot.ClassificationReport.from_raw_data(
+        y_test, y_pred_rf, target_names=target_names
+    )
+    cr_lr = plot.ClassificationReport.from_raw_data(
+        y_test, y_pred_lr, target_names=target_names
+    )
+
+    # compare both reports
+    show_figure((cr_rf + cr_lr).figure)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
