b56d514896b54d7f335e7a5bba371d53cd5c9901
Neelasha Sen <neelasha.sen@gmail.com>
2022-12-27 23:30:13 +0530

    fixes error when passing array to `table.feature_importances` (#144)
    * array
    
    * lint
    
    * lint
    
    * review comment
    
    Co-authored-by: Neelasha Sen <neelashasen@192.168.1.2>

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,5 +1,6 @@
 from unittest.mock import Mock
 
+import pytest
 import numpy as np
 from sklearn_evaluation import table
 
@@ -26,14 +27,19 @@ def test_feature_importances_top3(path_to_tests_static):
     assert expected == str(table.feature_importances(ft_imp, top_n=3))
 
 
-def test_feature_importances_feature_names(path_to_tests_static):
+@pytest.mark.parametrize(
+    "feature_names",
+    [["thing_a", "thing_b", "thing_c", "thing_d", "thing_e"],
+     np.array(["thing_a", "thing_b", "thing_c", "thing_d", "thing_e"])],
+    ids=["list", "array"])
+def test_feature_importances_feature_names(path_to_tests_static,
+                                           feature_names):
     feature_importances = np.array([0.12, 0.10, 0.8, 0.06, 0.03])
-    feature_names = ["thing_a", "thing_b", "thing_c", "thing_d", "thing_e"]
     with open(path_to_tests_static / "table_ft_names.txt", "r") as f:
         expected = f.read()
     assert expected == str(
-        table.feature_importances(feature_importances, feature_names=feature_names)
-    )
+        table.feature_importances(feature_importances,
+                                  feature_names=feature_names))
 
 
 def test_feature_importances_w_subestimators(path_to_tests_static):
@@ -58,29 +64,24 @@ def test_extend_to():
 
 
 def test_fixed_length_lists():
-    assert table.fixed_length_lists(
-        [
-            [1, 2, 3],
-            [],
-            [1],
-            [1, 2],
-        ]
-    ) == [[1, 2, 3], [None, None, None], [1, None, None], [1, 2, None]]
+    assert table.fixed_length_lists([
+        [1, 2, 3],
+        [],
+        [1],
+        [1, 2],
+    ]) == [[1, 2, 3], [None, None, None], [1, None, None], [1, 2, None]]
 
 
 def test_from_columns():
-    t = table.Table.from_columns(
-        content=[[1, 2, 3], [4], [5, 6]], header=["a", "b", "c"]
-    )
+    t = table.Table.from_columns(content=[[1, 2, 3], [4], [5, 6]],
+                                 header=["a", "b", "c"])
 
-    assert str(t) == (
-        "+-----+-----+-----+"
-        "\n|   a |   b |   c |"
-        "\n+=====+=====+=====+"
-        "\n|   1 |   4 |   5 |"
-        "\n+-----+-----+-----+"
-        "\n|   2 |     |   6 |"
-        "\n+-----+-----+-----+"
-        "\n|   3 |     |     |"
-        "\n+-----+-----+-----+"
-    )
+    assert str(t) == ("+-----+-----+-----+"
+                      "\n|   a |   b |   c |"
+                      "\n+=====+=====+=====+"
+                      "\n|   1 |   4 |   5 |"
+                      "\n+-----+-----+-----+"
+                      "\n|   2 |     |   6 |"
+                      "\n+-----+-----+-----+"
+                      "\n|   3 |     |     |"
+                      "\n+-----+-----+-----+")
