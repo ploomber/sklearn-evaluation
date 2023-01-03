1174903d849cbdfc5dbe6fb0f5ca9d4521fbd1f8
digithed <nickrosenau1@gmail.com>
2023-01-03 17:56:39 -0500

    fixes.

--- examples/confusion_matrix_oop.py
+++ examples/confusion_matrix_oop.py
@@ -2,8 +2,16 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+ 
+import os
+import sys
+import inspect
 
-from sklearn_evaluation import plot
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+from src.sklearn_evaluation import plot
 
 X, y = datasets.make_classification(
     1000, 20, n_informative=10, class_sep=0.80, n_classes=3, random_state=0
@@ -20,7 +28,7 @@ tree_cm = plot.ConfusionMatrix.from_raw_data(y_test, y_pred)
 est = DecisionTreeClassifier()
 est.fit(X_train, y_train)
 y_pred = est.predict(X_test)
-forest_cm = plot.ConfusionMatrix(y_test, y_pred)
+forest_cm = plot.ConfusionMatrix.from_raw_data(y_test, y_pred)
 
 # Compare
 tree_cm + forest_cm
