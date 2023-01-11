5ee55c5c88a9f9f5d9f395d3117c6b53c0274841
George <gtduncan@usc.edu>
2023-01-11 12:46:41 -0500

    validating elbow_curve input model

--- src/sklearn_evaluation/plot/clustering.py
+++ src/sklearn_evaluation/plot/clustering.py
@@ -116,6 +116,11 @@ def elbow_curve(
         raise TypeError(
             '"n_clusters" attribute not in classifier. ' "Cannot plot elbow method."
         )
+        
+    if not hasattr(clf, "score"):
+        raise AttributeError(
+            "clf does not have a score method. Ensure it's one of the following: KMeans, MiniBatchKMeans, or BisectingKMeans"
+        )
 
     tuples = Parallel(n_jobs=n_jobs)(
         delayed(_clone_and_score_clusterer)(clf, X, i) for i in range_n_clusters
