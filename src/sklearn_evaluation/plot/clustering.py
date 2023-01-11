c98d8a75cf1370cf3a53b56244eaecc1a22337d7
George <gtduncan@usc.edu>
2023-01-11 17:27:04 -0500

    edited doc, fixed trailing whitespace

--- src/sklearn_evaluation/plot/clustering.py
+++ src/sklearn_evaluation/plot/clustering.py
@@ -116,10 +116,10 @@ def elbow_curve(
         raise TypeError(
             '"n_clusters" attribute not in classifier. ' "Cannot plot elbow method."
         )
-    
+
     if not hasattr(clf, "score"):
         raise AttributeError(
-            """clf does not have a score method. Ensure it's one of the following: 
+            """clf does not have a score method. Ensure it's one of the following:
             KMeans, MiniBatchKMeans, or BisectingKMeans"""
         )
 
