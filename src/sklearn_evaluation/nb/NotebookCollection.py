d84b205cb497f2d3e879202cfa930e15c7420936
Eduardo Blancas <github@blancas.io>
2023-01-11 16:42:42 -0600

    doc build fails on warnings + warning fixes

--- src/sklearn_evaluation/nb/NotebookCollection.py
+++ src/sklearn_evaluation/nb/NotebookCollection.py
@@ -28,7 +28,7 @@ class NotebookCollection(Mapping):
     To access output, notebooks must tag the cells (one tag per cell). For
     instructions on tagging cells, `see this <https://papermill.readthedocs.io/en/latest/usage-parameterize.html>`_ # noqa
 
-    :doc:`Click here <../nbs/NotebookCollection>` to see the user guide.
+    :doc:`Click here <../comparison/NotebookCollection>` to see the user guide.
 
     Parameters
     ----------
