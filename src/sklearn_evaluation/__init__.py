0519a5a40c14655d7db37acb043c38c9679e5e88
Eduardo Blancas <github@blancas.io>
2023-01-31 09:40:50 -0300

    fixes inline plots in tutorial (#260)
    * fixes inline plots in tutorial
    
    * migrates ClassificationReport to AbstractPlot
    
    * fixes doctest
    
    * applies pending deprecationt to classification report
    
    * fixes changelog

--- src/sklearn_evaluation/__init__.py
+++ src/sklearn_evaluation/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.10.1dev"
+__version__ = "0.11.0dev"
 
 from sklearn_evaluation.evaluator import ClassifierEvaluator
 from sklearn_evaluation.nb.NotebookIntrospector import NotebookIntrospector
