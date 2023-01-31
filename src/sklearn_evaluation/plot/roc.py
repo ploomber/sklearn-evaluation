62ef114eddd5b4904e4142d214595aa430e62906
George <gtduncan@usc.edu>
2023-01-31 14:59:59 -0500

    turned curve 2 into dotted line

--- src/sklearn_evaluation/plot/roc.py
+++ src/sklearn_evaluation/plot/roc.py
@@ -204,7 +204,7 @@ def _roc_curve_multi(y_true, y_score):
     return roc_curve(y_true.ravel(), y_score.ravel())
 
 
-def _plot_roc(fpr, tpr, ax, label=None):
+def _plot_roc(fpr, tpr, ax, label=None, linestyle=None):
     """
     Plot ROC curve
 
@@ -234,13 +234,13 @@ def _plot_roc(fpr, tpr, ax, label=None):
 
     label = label or "ROC curve"
 
-    ax.plot(fpr, tpr, label=(f"{label} (area = {roc_auc:0.2f})"))
+    ax.plot(fpr, tpr, label=(f"{label} (area = {roc_auc:0.2f})"), linestyle=linestyle)
 
     _set_ax_settings(ax)
     return ax
 
 
-def _generate_plot_from_fpr_tpr_lists(fpr, tpr, ax, label=None):
+def _generate_plot_from_fpr_tpr_lists(fpr, tpr, ax, label=None, linestyle=None):
     """
     Draws a plot for every list of values i.e tpr[i] and fpr[i].
     """
@@ -248,7 +248,8 @@ def _generate_plot_from_fpr_tpr_lists(fpr, tpr, ax, label=None):
         fpr_ = fpr[i]
         tpr_ = tpr[i]
         label_ = label[i] if label is not None and len(label) > 0 else None
-        _plot_roc(fpr_, tpr_, ax, label=label_)
+
+        _plot_roc(fpr_, tpr_, ax, label=label_, linestyle=linestyle)
 
 
 class ROCAdd(AbstractComposedPlot):
@@ -291,7 +292,7 @@ class ROCAdd(AbstractComposedPlot):
         else:
             b_label = ["ROC curve 2"]
 
-        _generate_plot_from_fpr_tpr_lists(b.fpr, b.tpr, ax, label=b_label)
+        _generate_plot_from_fpr_tpr_lists(b.fpr, b.tpr, ax, label=b_label, linestyle='dotted')
 
         self.ax_ = ax
         self.figure_ = ax.figure
