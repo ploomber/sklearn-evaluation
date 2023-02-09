d457ff56a516fd52aa505e24242d529c7a9dea86
yafimvo <yafimvo@gmail.com>
2023-02-09 19:56:34 +0200

    docs fixed

--- src/sklearn_evaluation/plot/style.py
+++ src/sklearn_evaluation/plot/style.py
@@ -4,18 +4,27 @@ import matplotlib as mpl
 
 
 @contextmanager
-def tmp_theme():
+def tmp_theme(ax=None):
+    """
+    Adds a scheme coloring and styling to matplotlib plots.
+    """
     try:
         _set_default_plot_colors()
         yield
     finally:
-        _set_default_ax_style()
+        _set_default_ax_style(ax)
 
 
 def apply_theme(func):
+    """
+    Decorates `def plot(self, ax = None)` and applies a visual theme to matplotlib plots.
+    """
     def wrapper_func(*args, **kwargs):
-        with tmp_theme():
-            func(*args, **kwargs)
+        # we assume the plotting function is ``def plot(self, ax = None)``
+        ax = args[1] if len(args) > 1 else None
+
+        with tmp_theme(ax):
+            return func(*args, **kwargs)
 
     return wrapper_func
 
@@ -34,8 +43,9 @@ def _set_default_plot_colors():
     mpl.rcParams["axes.prop_cycle"] = mpl.cycler(color=_material_ui_colors)
 
 
-def _set_default_ax_style():
-    ax = plt.subplot()
+def _set_default_ax_style(ax=None):
+    if ax is None:
+        ax = plt.subplot()
     ax.set_xlim([0.0, 1.005])
     ax.set_ylim([0.0, 1.005])
     ax.legend(loc="best", fontsize="10")
