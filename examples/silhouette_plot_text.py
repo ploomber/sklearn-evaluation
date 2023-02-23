878e8d4e7c6d1a9f08ccd3d392c319cf223cd68f
idomi <michael.ido@gmail.com>
2023-02-23 12:20:06 -0500

    pytest on /tests only

--- examples/silhouette_plot_text.py
+++ examples/silhouette_plot_text.py
@@ -20,7 +20,6 @@ categories = [
 dataset = fetch_20newsgroups(
     remove=("headers", "footers", "quotes"),
     subset="all",
-    data_home=".",
     categories=categories,
     shuffle=True,
     random_state=42,
