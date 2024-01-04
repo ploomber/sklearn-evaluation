c0344d2033d92d37e16ccb3588f752465ac1ab6a
neelasha23 <neelasha.sen@gmail.com>
2024-01-04 21:28:27 +0530

    Remove binder

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,6 @@ html_theme_options = {
     "search_bar_text": "Search this book...",
     "launch_buttons": {
         "notebook_interface": "jupyterlab",
-        "binderhub_url": "",
         "jupyterhub_url": "",
         "thebe": False,
         "colab_url": "",
